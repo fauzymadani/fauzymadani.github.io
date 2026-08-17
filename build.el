9fc6d9ca2174b44578395a93e76d24b35ace0643
fauzymadani <keperluansekolahfauzy@gmail.com>
2026-08-17 09:40:51 +0700

    change build script

--- build.el
+++ build.el
@@ -1,8 +1,24 @@
 ;;; build.el --- ponytail:ox-publish build script -*- lexical-binding: t; -*-
 (require 'ox-publish)
 
+(defvar my-site-title "Fauzy's Blog")
+
+(defvar my-nav-links
+  '(("Home"    . "index.html")
+    ("Archive" . "archive.html")))
+
+(defun my-render-nav ()
+  "Generate HTML navigation markup from `my-nav-links`."
+  (concat
+   (format "<nav class=\"site-nav\"><a href=\"index.html\" class=\"nav-title\">%s</a>" my-site-title)
+   (mapconcat (lambda (link)
+                (format "<a href=\"%s\">%s</a>" (cdr link) (car link)))
+              my-nav-links
+              "")
+   "</nav>"))
+
 (setq org-publish-project-alist
-      '(("blog-content"
+      `(("blog-content"
          :base-directory "./content"
          :base-extension "org"
          :publishing-directory "./public"
@@ -20,7 +36,7 @@
          :html-head-include-default-style nil
          :html-head-include-scripts nil
          :html-head "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\"/>"
-         :html-preamble "<nav class=\"site-nav\"><a href=\"index.html\" class=\"nav-title\">Fauzy's Blog</a><a href=\"index.html\">Home</a><a href=\"archive.html\">Archive</a></nav>"
+         :html-preamble ,(my-render-nav)
          :html-postamble "<div class=\"footer-content\"><p>© %a · Written in Org-mode</p><a href=\"https://notbyai.fyi\" target=\"_blank\" rel=\"noopener\"><img src=\"images/not-by-ai.svg\" alt=\"Written by a Human, Not by AI\" class=\"not-by-ai-badge\" /></a></div>")
         ("blog-static"
          :base-directory "./content"
